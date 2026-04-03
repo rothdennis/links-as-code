ef22e11dde133612515e9c5f8ca1853d0497dde3
Dennis Roth <rothdennis92@gmail.com>
2026-04-03 18:28:50 +0200

    Update Dockerfile and main.py for improved configuration and add docker-compose support

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,17 @@ FROM python:3.14-alpine3.23
 
 RUN apk add nginx
 COPY configs/nginx.conf /etc/nginx/nginx.conf
+RUN nginx -g 'daemon off;' &
 
 WORKDIR /app
+RUN mkdir -p /app/data
 
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 COPY src/ .
 
-EXPOSE 5050
+ENV DATA_DIR=/app/data
+
+EXPOSE 8000
 
 CMD ["python", "src/main.py"]
--- src/main.py
+++ src/main.py
@@ -4,14 +4,18 @@ import yaml
 
 DEBUG = os.getenv('DEBUG', 'True').lower() in ('true', '1', 't', 'yes', 'y')
 PORT = 5050
-FILE_PATH = os.getenv('FILE_PATH', './data/links.yaml')
+DATA_DIR = os.getenv('DATA_DIR', './data')
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    with open(FILE_PATH, 'r') as file:
-        links = yaml.safe_load(file)
+    # Load the first YAML file in DATA_DIR
+    for filename in os.listdir(DATA_DIR):
+        if filename.endswith('.yaml') or filename.endswith('.yml'):
+            with open(os.path.join(DATA_DIR, filename), 'r') as f:
+                links = yaml.safe_load(f)
+            break
     return render_template('index.html', data=links)
 
 
